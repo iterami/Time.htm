250db58fb7f4f6a42e72ecd01ea9c529aac746a2
honzi <czechinthemail@gmail.com>
2017-03-01 00:52:16 -0800

    switched day to date

--- js/time.js
+++ js/time.js
@@ -8,16 +8,19 @@ function second(){
     var timestamp = time_timestamp_to_date();
     document.getElementById('timestamp-current').value = timestamp['timestamp'];
 
-    document.getElementById('date').innerHTML = time_format_date({
+    document.getElementById('date-display').innerHTML = time_format_date({
       'date': timestamp,
     });
 }
 
 function update_date_inputs(date){
     for(var portion in date){
-        document.getElementById(portion).value = time_two_digits({
-          'number': date[portion],
-        });
+        var element = document.getElementById(portion);
+        if(element){
+            element.value = time_two_digits({
+              'number': date[portion],
+            });
+        }
     }
 }
 
@@ -30,7 +33,7 @@ window.onload = function(e){
 
     document.getElementById('date-to-timestamp').onclick = function(){
         var date = {
-          'day': parseInt(document.getElementById('day').value, 10),
+          'date': parseInt(document.getElementById('date').value, 10),
           'hour': parseInt(document.getElementById('hour').value, 10),
           'minute': parseInt(document.getElementById('minute').value, 10),
           'month': parseInt(document.getElementById('month').value, 10),
