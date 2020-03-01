40b061934692e2334f3fbd35488aa84c3cc18ba1
Jan Hořava <czechinthemail@gmail.com>
2020-03-01 11:10:05 -0800

    absorbed iterami/Alarm.htm

--- js/data.js
+++ js/data.js
@@ -1,5 +1,69 @@
 'use strict';
 
+function alarm_clear(element, id){
+    document.getElementById('alarms-table').removeChild(
+      element.parentElement.parentElement
+    );
+
+    entity_remove({
+      'entities': [
+        id,
+      ],
+    });
+
+    core_storage_data['alarms'] = JSON.stringify(entity_entities);
+    core_storage_update();
+}
+
+function alarm_create(args){
+    args = core_args({
+      'args': args,
+      'defaults': {
+        'label': document.getElementById('alarm-label').value,
+        'remake': false,
+        'target': date_to_timestamp() + Number.parseInt(document.getElementById('alarm-seconds').value, 10) * 1000,
+      },
+    });
+
+    if(!args['remake']
+      && JSON.parse(core_storage_data['alarms'])[args['label']]){
+        return;
+    }
+
+    entity_create({
+      'id': args['label'],
+      'properties': {
+        'label': args['label'],
+        'target': args['target'],
+      },
+      'types': [
+        'alarm',
+      ],
+    });
+
+    document.getElementById('alarms-table').insertAdjacentHTML(
+      'beforeend',
+      '<tr id="' + args['label'] + '">'
+        + '<td>' + args['label']
+        + '<td>'
+        + '<td>' + time_format({
+          'date': timestamp_to_date({
+            'timestamp': entity_entities[args['label']]['target'],
+          }),
+        })
+        + '<td><input checked type=checkbox><input id="' + args['label'] + '-button" type=button value=X>'
+    );
+    document.getElementById(args['label'] + '-button').onclick = function(){
+        alarm_clear(
+          this,
+          args['label']
+        );
+    };
+
+    core_storage_data['alarms'] = JSON.stringify(entity_entities);
+    core_storage_update();
+}
+
 function update(){
     const timestamp = timestamp_to_date();
     document.getElementById('timestamp-current').value = timestamp['timestamp'];
@@ -21,6 +85,35 @@ function update(){
     for(const id in diffs){
         document.getElementById('diff-' + id).textContent = core_number_format({
           'number': diff / diffs[id],
+        });
+    }
+
+    let play_alarm_sound = false;
+    entity_group_modify({
+      'groups': [
+        'alarm',
+      ],
+      'todo': function(entity){
+          const element = document.getElementById(entity);
+          const remaining = (entity_entities[entity]['target'] - date_to_timestamp()) / 1000;
+
+          element.childNodes[1].textContent = time_diff({
+            'target': remaining * 1000 + date_to_timestamp(),
+          });
+
+          if(remaining < 0){
+              element.style.background = '#f00';
+
+              if(element.childNodes[3].childNodes[0].checked){
+                  play_alarm_sound = true;
+              }
+          }
+      },
+    });
+
+    if(play_alarm_sound){
+        audio_start({
+          'id': 'alarm',
         });
     }
 }
--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@
 
 function repo_init(){
     core_repo_init({
+      'beforeunload': {
+        'todo': function(){
+            if(entity_info['alarm']['count'] <= 0){
+                document.getElementById('alarms').value = '{}';
+            }
+
+            core_storage_save();
+        }
+      },
       'events': {
+        'add-alarm': {
+          'onclick': function(){
+              alarm_create();
+          },
+        },
         'date-to-timestamp': {
           'onclick': function(){
               update_times(time_from_inputs());
@@ -19,9 +33,33 @@ function repo_init(){
           },
         },
       },
+      'storage': {
+        'alarms': '{}',
+      },
+      'storage-menu': '<textarea id=alarms></textarea><br>',
       'title': 'Time.htm',
     });
+    entity_set({
+      'default': true,
+      'type': 'alarm',
+    });
+    audio_create({
+      'audios': {
+        'alarm': {
+          'duration': .5,
+          'frequency': 666,
+        },
+      },
+    });
 
+    const alarms = JSON.parse(core_storage_data['alarms']);
+    for(const alarm in alarms){
+        alarm_create({
+          'label': alarms[alarm]['label'],
+          'remake': true,
+          'target': alarms[alarm]['target'],
+        });
+    }
     update_times(timestamp_to_date()['timestamp']);
 
     core_interval_modify({
